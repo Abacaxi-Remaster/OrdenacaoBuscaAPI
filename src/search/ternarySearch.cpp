cea128a6d804c1f06afd4ec3d60918ea7f2239e0
André Cano <andrerenatofc@gmail.com>
2023-04-17 15:00:47 -0300

    ternarySearch Implemented

---
+++ src/search/ternarySearch.cpp
@@ -0,0 +1,32 @@
+int main ()
+{
+    return 0;
+}
+
+int tSearch(int *array, int value, int start, int end)
+{
+    if (end > start)
+    {
+        int mid1 = start + (end - start) / 3;
+        int mid2 = end - (end - start) / 3;
+        if (array[mid1] == value)
+        {
+            return mid1;
+        }
+        if (array[mid2] == value)
+        {
+            return mid2;
+        }
+
+        if (array[mid1] > value)
+        {
+            return tSearch(array, value, start, mid1 - 1);
+        }
+        else if (array[mid2] > value)
+        {
+            return tSearch(array, value, mid1, mid2 - 1);
+        }
+        return tSearch(array, value, mid2, end);
+    }
+    return -1;
+}
