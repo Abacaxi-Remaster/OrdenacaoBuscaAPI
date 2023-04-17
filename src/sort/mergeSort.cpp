f4b9d4ce3d615163cb9eaf97ef31ce21027296bd
André Cano <andrerenatofc@gmail.com>
2023-04-17 14:26:26 -0300

    mergeSort implemented

---
+++ src/sort/mergeSort.cpp
@@ -0,0 +1,62 @@
+int main()
+{
+    return 0;
+}
+
+void merge(int *array, int left, int center, int right)
+{
+    int sizel = center - left + 1; // tamanho da primeira parte
+    int sizer = right - center;    // tamanho da segunda parte
+    int L[sizel] = {};
+    int R[sizer] = {};
+
+    for (int i = 0; i < sizel; i++)
+    {
+        L[i] = array[left + i]; // preenche a metade esquerda
+    }
+    for (int j = 0; j < sizer; j++)
+    {
+        R[j] = array[center + j + 1]; // preenche a metade direita
+    }
+    int i = 0;
+    int j = 0;
+    int k = left;
+    while (i < sizel && j < sizer)
+    {
+        if (L[i] <= R[j])
+        {
+            array[k] = L[i];
+            i++;
+        }
+        else
+        {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < sizel)
+    {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < sizer)
+    {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mSort(int *array, int start, int end)
+{
+    if (start < end)
+    {
+        int center = (start + end) / 2;
+        mSort(array, start, center);
+        mSort(array, center + 1, end);
+        merge(array, start, center, end);
+    }
+}
