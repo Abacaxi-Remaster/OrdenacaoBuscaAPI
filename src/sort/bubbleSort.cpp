c7c501b7bd8913f31b3e4e0616f492ba6b2e275b
André Cano <andrerenatofc@gmail.com>
2023-04-17 14:23:59 -0300

    Swap function and Bubble sort implemented

---
+++ src/sort/bubbleSort.cpp
@@ -0,0 +1,22 @@
+#include "swap.hpp"
+int main()
+{
+    return 0;
+}
+
+void bubbleSort(int *array, int length)
+{
+    bool swapped;
+    do
+    {
+        swapped = false;
+        for (int i = 0; i < length - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap(array[i], array[i + 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
