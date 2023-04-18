d55b7bc8257f67602fa1d4855b522c014c86ea61
André Cano <andrerenatofc@gmail.com>
2023-04-18 18:06:32 -0300

    qSort main implemented

--- src/search/binarySearch.cpp
+++ src/search/binarySearch.cpp
@@ -4,8 +4,8 @@ int bSearch(int *array, int value, int start, int end);
 
 int main(int argc, char *argv[])
 {
-    int value = stoi(argv[1]);
-    int length = stoi(argv[2]);
+    int length = stoi(argv[1]);
+    int value = stoi(argv[2]);
     int *array = fileToArray(length, "../array.csv");
     qSort(array, 0, length - 1);
     cout << bSearch(array, value, 0, length - 1) << endl;
--- src/sort/quickSort.cpp
+++ src/sort/quickSort.cpp
@@ -1,32 +1,9 @@
-#include "swap.hpp"
+#include "../fileToArray.hpp"
 
-void qSort(int arr[], int start, int end);
-int partition(int *array, int l, int r);
-
-int partition(int *array, int l, int r)
+int main(int argc, char *argv[])
 {
-    int pivot = array[r];
-    int i = l;
-    for (int j = l; j < r; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            if (i != j)
-            {
-                swap(array[i], array[j]);
-            }
-            i++;
-        }
-    }
-    swap(array[i], array[r]);
-    return i;
-}
-void qSort(int arr[], int start, int end)
-{
-    if (start < end)
-    {
-        int p = partition(arr, start, end);
-        qSort(arr, start, p - 1);
-        qSort(arr, p + 1, end);
-    }
+    int length = stoi(argv[1]);
+    int *array = fileToArray(length, "../array.csv");
+    qSort(array, 0, length - 1);
+    return 0;
 }
