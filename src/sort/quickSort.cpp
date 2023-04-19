81a2f8347209772a24b2beb94281125ba94d503d
André Cano <andrerenatofc@gmail.com>
2023-04-18 22:28:36 -0300

    fixed linux executables
    fixed quicksort timer

--- src/sort/quickSort.cpp
+++ src/sort/quickSort.cpp
@@ -7,9 +7,10 @@ int main(int argc, char *argv[])
 {
     int length = stoi(argv[1]);
     int *array = fileToArray(length, "data/array.csv");
-    qSort(array, 0, length - 1);
 
     auto start = steady_clock::now();
+
+    qSort(array, 0, length - 1);
 
     auto end = steady_clock::now();
     duration<double, std::micro> time = end - start;
