27c86710f9186daae30f3471091f681b028feda1
André Cano <andrerenatofc@gmail.com>
2023-04-19 23:01:39 -0300

    running stuff on server per client request

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 // App Express
 const app = express()
-// Endpoint raiz
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// endpoint raiz
 app.get('/', (req, res) => {
     var fileName = "/home.html";
     res.sendFile(__dirname + fileName, function (err) {
@@ -18,14 +21,36 @@ app.get('/', (req, res) => {
     })
     return;
 })
+
+// endpoint raiz
+app.get('/favicon.ico', (req, res) => {
+    var fileName = "/favicon.ico";
+    res.sendFile(__dirname + fileName, function (err) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    })
+    return;
+})
+
+app.post('/run', (req, res) => {
+    console.log("Teste");
+    res.end();
+})
+
 // Cors
 app.use(cors({
     origin: ['http://localhost:3000']
 }))
+
 // Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {
-    res.status(404)
+    console.log(req);
+    res.status(404);
 })
+
 // Inicia o sevidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
